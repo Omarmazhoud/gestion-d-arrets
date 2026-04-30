b591af6328580ed4f08bf2c0ba51d4f44ad7705f
Omar Mazhoud <tonemail@example.com>
2026-04-30 12:03:56 +0100

    Utilisation de variables d'environnement pour l'API URL

--- react-admis/src/services/api.js
+++ react-admis/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.20.10.2:8080/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://172.20.10.2:8080/api",
 });
 
 export default api;
