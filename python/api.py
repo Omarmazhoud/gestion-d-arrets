34809e883a417d3eb4530efa2c12189bc6d31c22
Omar Mazhoud <tonemail@example.com>
2026-05-06 22:52:21 +0100

    Fix IA : Utilisation de la dernière version de TensorFlow pour corriger le bug de BatchNormalization Keras 3

--- python/api.py
+++ python/api.py
@@ -20,7 +20,6 @@ except:
 # Charger le modèle Computer Vision
 try:
     import os
-    os.environ['TF_USE_LEGACY_KERAS'] = '1'
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Désactiver logs inutiles
 
     import tensorflow as tf
